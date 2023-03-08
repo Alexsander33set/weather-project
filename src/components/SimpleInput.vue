6b0cb5e65583b1cc7c1b76b13e04c8bc2542ed76
Alexsander P.F. Santos <82063944+Alexsander33set@users.noreply.github.com>
2023-03-07 23:38:24 -0300

    _
    Style on scrollbar
    Import main.css
    New input sample

---
+++ src/components/SimpleInput.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "SimpleInput",
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  props: {
+    placeholder: String,
+    type: { String, default: "text" },
+    value: { String, default: "" },
+    maxCaracters: { Number, default: 100 },
+  },
+  methods: {
+    inputValidating(e) {
+      var pattern = "[a-zA-Z0-9]";
+      const char = String.fromCharCode(e.keyCode);
+      if (char.match(pattern)) {
+        console.log(char);
+        return true;
+      } else {
+        console.log(char);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    :type="type"
+    :value="this.value"
+    placeholder="Cidade"
+    pattern="^[a-zA-Z0-9]+$"
+    @keydown="inputValidating($event)"
+    @keypress.enter="this.getWeatherByCity()"
+  />
+</template>
